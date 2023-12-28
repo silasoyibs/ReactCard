b1b1a208b698cd7a6a42383bdd9b189be0756b88
silasoyibs <silasoyibs@gmail.com>
2023-12-28 12:19:39 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Image />
+      <div className="data">
+        <Tittle />
+        <Body />
+        <div className="skill-list">
+          <SkillList name="Html+Css" emoji="👌" backgroundColor="red" />
+          <SkillList name="Javascript" emoji="🎊" backgroundColor="blue" />
+          <SkillList name="Web design" emoji="✔" backgroundColor="orange" />
+          <SkillList name="Git and Github" emoji="⛔" backgroundColor="green" />
+          <SkillList name="React" emoji="🍕" backgroundColor="yellow" />
+        </div>
+      </div>
+    </div>
+  );
+}
+function Image() {
+  return (
+    <img
+      src="WhatsApp Image 2023-12-07 at 6.18.23 PM.jpeg"
+      alt=""
+      class="avatar"
+    ></img>
+  );
+}
+function Tittle() {
+  return <h1>Jonas Schmedtmann</h1>;
+}
+
+function Body() {
+  return (
+    <p>
+      Full-stack web developer and teacher at udemy. when not coding or
+      preparing a course, i like to play board games, to cook
+    </p>
+  );
+}
+
+function SkillList(props) {
+  return (
+    <div class="skill" style={{ backgroundColor: props.backgroundColor }}>
+      {props.name}
+      {props.emoji}
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
